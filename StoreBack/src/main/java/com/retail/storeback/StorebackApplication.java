eaaf02fef91bcfc80b195f2c6ff6453e6dfa2890
Grenbui <buidat1131@gmail.com>
2026-08-12 22:16:19 +0700

    init project

---
+++ StoreBack/src/main/java/com/retail/storeback/StorebackApplication.java
@@ -0,0 +1,14 @@
+package com.retail.storeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorebackApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StorebackApplication.class, args);
+	}
+
+}
